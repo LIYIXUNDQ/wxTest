83809c740eac77a5e6b0ecbbf470f479d4ba347c
LIYIXUNDQ <48639898+LIYIXUNDQ@users.noreply.github.com>
2019-05-06 15:53:32 +0800

    Add files via upload

---
+++ 四/components/pmdggl/pmdggl.js
@@ -0,0 +1,33 @@
+// components/pmdggl/pmdggl.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+      imagePath: "/images/电脑一.jpg",
+      pagePath: "/pages/lxjsq/lxjsq"
+    }, {
+        imagePath: "/images/电脑二.jpg",
+      pagePath: "/pages/lxjsq/lxjsq"
+    }, {
+        imagePath: "/images/电脑三.jpg",
+      pagePath: "/pages/lxjsq/lxjsq"
+    }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
